56e5af8c108be7cdd859de189fc8a81839fd8703
LocrioPerdio <129734290+LocrioPerdio@users.noreply.github.com>
2026-05-19 19:35:50 +0200

    utils updated

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 19:32:21 by paduarte          #+#    #+#             */
-/*   Updated: 2026/05/17 21:23:56 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/19 18:37:27 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 3
 # endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strchr(const char *s, int c);
+size_t  ft_strlen(const char *s);
+char    *ft_strdup(const char *s);
+
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,74 @@
 /*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 19:32:12 by paduarte          #+#    #+#             */
-/*   Updated: 2026/05/17 19:36:14 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/19 18:34:24 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char    *ft_strjoin(char const *s1, char const *s2)
+{
+        char    *s3;
+        size_t  i;
+        size_t  j;
+
+        i = 0;
+        j = 0;
+        s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+        if (!s3)
+                return (NULL);
+        while (s1[i])
+        {
+                s3[i] = s1[i];
+                i++;
+        }
+        while (s2[j])
+        {
+                s3[i] = s2[j];
+                j++;
+                i++;
+        }
+        s3[i] = '\0';
+        return (s3);
+}
+
+char    *ft_strchr(const char *s, int c)
+{
+        while (*s)
+        {
+                if ((unsigned char)*s == (unsigned char)c)
+                        return ((char *)s);
+                s++;
+        }
+        if ((unsigned char)c == '\0')
+                return ((char *)s);
+        return (NULL);
+}
+
+size_t  ft_strlen(const char *s)
+{
+        size_t  count;
+
+        count = 0;
+        while (s[count])
+                count++;
+        return (count);
+}
+char    *ft_strdup(const char *s)
+{
+        char    *dup;
+        size_t  i;
+
+        i = 0;
+        dup = malloc(ft_strlen(s) + 1);
+        if (!dup)
+                return (NULL);
+        while (s[i])
+        {
+                dup[i] = s[i];
+                i++;
+        }
+        dup[i] = '\0';
+        return (dup);
+}
