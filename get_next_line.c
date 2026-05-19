8e5085d29189bf10d99a6906f07a6a84723ee158
LocrioPerdio <129734290+LocrioPerdio@users.noreply.github.com>
2026-05-19 19:35:09 +0200

    read_and_store, extract_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,85 @@
 /*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 19:32:17 by paduarte          #+#    #+#             */
-/*   Updated: 2026/05/17 20:50:52 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/19 19:34:10 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 // Is there space to incorporate ft_calloc instead of malloc?
 
+static char *ft_read_and_store(int fd, char *buffer);
+static char *ft_extract_line(char *buffer);
+
 char	*get_next_line(int fd)
 {
-	int		bytes_read;
-	char	*buffer;
+	static char	*buffer;
+	char *line;
 
+    if (!buffer)
+		buffer = ft_strdup("");
+	line = NULL;
+	buffer = ft_read_and_store(fd, buffer);
+	line = ft_extract_line(buffer);
+	return (line);
+}
+
+static char *ft_extract_line(char *buffer)
+{
+	char *line;
+	size_t i;
+	size_t j;	
+	
+	i = 0;
+	j = 0;
+	line = NULL;
+	while(buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return(NULL);
+	while(j < i)
+	{
+		line[j] = buffer[j];
+		j++;
+	}
+	line[j] = '\0';	
+	return(line);
+}
+
+static char *ft_read_and_store(int fd, char *buffer)
+{
+	int		bytes_read;
+	char	*tmp;
+	char	*total;
+
+	bytes_read = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
+	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp)
 		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
+	while(bytes_read > 0 && !ft_strchr(buffer, '\n'))
 	{
-		free(buffer);
+		bytes_read = read(fd, tmp, BUFFER_SIZE);
+	if (bytes_read < 0)
+	{
+		free(tmp);
 		return (NULL);
 	}
-	// buffer[bytes_read] = '\0';
+	tmp[bytes_read] = '\0';
+	total = ft_strjoin(buffer, tmp);
+	buffer = total;
+	free(buffer);
+}
+	free(tmp);
 	return (buffer);
 }
+
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -45,12 +97,12 @@ int	main(void)
 
 	fd = open("example.txt", O_RDONLY);
 	count = 0;
-	while (1)
+	while (count < 2)
 	{
 		next_line = get_next_line(fd);
 		count++;
 		if (next_line == NULL)
-			break ;
+			break;
 		printf("[%d]:%s\n", count, next_line);
 		free(next_line);
 	}
